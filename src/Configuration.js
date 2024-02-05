77d3398d81a3dbe931349058781a5ffcface6792
flaviovelecela <Velecelaf@usf.edu>
2024-02-04 22:55:45 -0500

    authentication changes

---
+++ src/Configuration.js
@@ -0,0 +1,9 @@
+const awsExports={
+    Auth:{
+        'userPoolId':'us-east-2_5Tj3Nt9Di',
+        'userPoolWebClientId':'5fhvbcbqi2n95pi9a7e3qjfsrs'
+        
+    }
+}
+
+export default awsExports
---
+++ src/Splashpage/home.jsx
@@ -0,0 +1,87 @@
+import React, {useState} from 'react';
+import './homepage.css'
+
+const homepage = () => {
+    return(
+        <div id="mainpage">
+      <div className="container">
+        <button id="loginBtn">Login</button>
+        <button id="registerBtn">Register</button>
+      </div>
+      <table>
+        <caption> All Games Played</caption>
+        <tbody>
+          <tr>
+            <th>#</th>
+            <th>Game Cover</th>
+            <th>Title</th>
+            <th>Achievement%</th>
+            <th>Rating</th>
+            <th>Status</th>
+          </tr>
+
+          <tr class="game-row">
+            <td class="game-number">1</td>
+            <td><img img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Hades_cover_art.jpg/220px-Hades_cover_art.jpg"
+              alt="Hades"></img></td>
+            <td>Hades</td>
+            <td>6%</td>
+            <td>N/A</td>
+            <td>Currently Playing</td>
+          </tr>
+
+          <tr class="game-row" data-status="on-hold">
+            <td class="game-number">2</td>
+            <td><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Celeste_box_art_full.png/1200px-Celeste_box_art_full.png"
+              alt="Celeste"></img></td>
+            <td>Celeste</td>
+            <td>100%</td>
+            <td>10/10</td>
+            <td>On Hold</td>
+          </tr>
+
+          <tr class="game-row" data-status="dropped">
+            <td class="game-number">3</td>
+            <td><img src="https://upload.wikimedia.org/wikipedia/en/6/6d/Bethesda_Starfield.jpg"
+              alt="Starfield"></img>
+            </td>
+            <td>Starfield</td>
+            <td>32%</td>
+            <td>N/A</td>
+            <td>Quit Playing</td>
+          </tr>
+
+          <tr class="game-row" data-status="planning">
+            <td class="game-number">4</td>
+            <td><img src="https://upload.wikimedia.org/wikipedia/en/8/85/Tunic_cover_art.jpg" alt="Tunic"></img>
+            </td>
+            <td>Tunic</td>
+            <td>7%</td>
+            <td>N/A</td>
+            <td>Planning to Play</td>
+          </tr>
+
+          <tr class="game-row" data-status="completed">
+            <td class="game-number">5</td>
+            <td><img src="https://upload.wikimedia.org/wikipedia/en/d/de/Lies_of_p_cover_art.jpg"
+              alt="Lies of P"></img>
+            </td>
+            <td>Lies of P</td>
+            <td>100%</td>
+            <td>10/10</td>
+            <td>Completed</td>
+          </tr>
+        </tbody>
+      </table>
+      <table id="footer">
+        <th><a href="#" onclick="return show('all');">All Games Played</a></th>
+        <th><a href="#" onclick="return show('completed');">Completed Games</a></th>
+        <th><a href="#" onclick="return show('planning');">Games Up Next</a></th>
+        <th><a href="#" onclick="return show('currently-playing');">Currently Playing</a></th>
+        <th><a href="#" onclick="return show('on-hold');">Games On Hold</a></th>
+        <th><a href="#" onclick="return show('dropped');">Games/Dropped Quit</a></th>
+      </table>
+    </div>
+    )
+}
+export default LoginSignup
