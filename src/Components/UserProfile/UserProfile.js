625dd7ca2a1d25ca02a31807a33f7ef87524dd9d
flaviovelecela <Velecelaf@usf.edu>
2024-03-24 13:58:52 -0400

    Added ability to edit display name

--- src/Components/UserProfile/UserProfile.js
+++ src/Components/UserProfile/UserProfile.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import './UserProfile.css'
-import { fetchUserAttributes } from 'aws-amplify/auth';
+import './UserProfile.css';
+import { fetchUserAttributes, updateUserAttribute } from 'aws-amplify/auth';
 
 function ProfilePage() {
   const [userAttributes, setUserAttributes] = useState({});
+  const [editMode, setEditMode] = useState(false);
+  const [newDisplayName, setNewDisplayName] = useState('');
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -12,34 +14,79 @@ function ProfilePage() {
       setIsLoading(true);
       try {
         const attributes = await fetchUserAttributes();
-        setUserAttributes({
-          email: attributes.email,
-          displayName: attributes['custom:DisplayName'],
-          steamId: attributes['custom:SteamID'],
-        });
+        setUserAttributes(attributes);
+        setNewDisplayName(attributes['custom:DisplayName'] || '');
       } catch (err) {
         console.error(err);
         setError(err.message);
+      } finally {
+        setIsLoading(false);
       }
-      setIsLoading(false);
     }
-
     getUserAttributes();
   }, []);
+
+  const handleEditClick = () => {
+    setEditMode(true);
+  };
+
+  const handleSaveClick = async () => {
+    try {
+      const output = await updateUserAttribute({
+        userAttribute: {
+          attributeKey: 'custom:DisplayName',
+          value: newDisplayName,
+        },
+      });
+      
+      setUserAttributes(prev => ({
+        ...prev,
+        'custom:DisplayName': newDisplayName,
+      }));
+      
+      setEditMode(false);
+    } catch (error) {
+      console.log('Error updating user attributes:', error);
+      setError(error.message || 'An error occurred');
+    }
+  };
+
+  // Handler for input change to update state
+  const handleChangeDisplayName = (e) => {
+    setNewDisplayName(e.target.value);
+  };
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
   if (error) {
-    return <div>Error fetching user attributes: {error}</div>;
+    return <div>Error: {error}</div>;
   }
 
   return (
     <div className="profile-page">
       <h1>My Profile</h1>
-      <p><strong>Username:</strong> {userAttributes.displayName}</p>
-      <p><strong>Steam ID:</strong> {userAttributes.steamId}</p>
+      <div>
+        <p><strong>Username:</strong>
+        {editMode ? (
+          <>
+            <input
+              type="text"
+              value={newDisplayName}
+              onChange={handleChangeDisplayName}
+            />
+            <button onClick={handleSaveClick}>Save</button>
+          </>
+        ) : (
+          <>
+            <span> {userAttributes['custom:DisplayName']}</span>
+            <button onClick={handleEditClick}>Edit</button>
+          </>
+        )}
+        </p>
+      </div>
+      <p><strong>Steam ID:</strong> {userAttributes['custom:SteamID']}</p>
     </div>
   );
 }
